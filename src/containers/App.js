99fc2fa1c6ab829b10790d5f078792e838071afb
evgner <evgner@gmail.com>
2019-01-26 18:50:35 +0600

    Add redux connect()

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import SearchBox from '../components/SearchBox';
 import CardList from '../components/CardList';
 // import { kitties } from './kitties';
@@ -6,13 +7,31 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
+import { setSearchField } from "../actions";
+/*import mapStateToProps
+	from "react-redux/es/connect/mapStateToProps";
+import mapDispatchToProps
+	from "react-redux/es/connect/mapDispatchToProps";*/
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchField
+	}
+};
+
+const mapDispatchToProps = ( dispatch ) => {
+	return {
+		onSearchChange: ( event ) => dispatch( setSearchField( event.target.value ) )
+	}
+};
+
 class App extends Component {
 
 	constructor() {
 		super();
 		this.state = {
-			kitties: [],
-			searchfield: ''
+			kitties: []
+			// searchfield: ''
 		}
 	}
 
@@ -25,21 +44,24 @@ class App extends Component {
 			.then( users => this.setState( { kitties: users } ) )
 	}
 
+/*
 	onSearchChange = ( event ) => {
 		this.setState( { searchfield: event.target.value } );
 	};
+*/
 
 	render() {
-		const { kitties, searchfield } = this.state;
+		const { kitties } = this.state;
+		const { searchField, onSearchChange } = this.props;
 		const filteredKitties = kitties.filter( kitty => {
-			return kitty.name.toLowerCase().includes( searchfield.toLowerCase() );
+			return kitty.name.toLowerCase().includes( searchField.toLowerCase() );
 		});
 		return ! kitties.length ?
 			<h1 className={'tc'}>Loading...</h1> :
 		(
 			<div className={'tc'}>
 				<h1 className={'f1'}>Kitty friends</h1>
-				<SearchBox searchChange={ this.onSearchChange }/>
+				<SearchBox searchChange={ onSearchChange } />
 				<Scroll>
 					<ErrorBoundry>
 						<CardList kitties={ filteredKitties } />
@@ -51,4 +73,4 @@ class App extends Component {
 
 }
 
-export default App;
+export default connect( mapStateToProps, mapDispatchToProps )( App );
