520a01dd5cbbf41fde7a381e3e641e1543eb7546
evgner <evgner@gmail.com>
2019-01-27 19:46:11 +0600

    Updates

---
+++ precache-manifest.42fa6e9a8cf7bca7764f2ce4cc299660.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kittyfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ecbdd3d7c969a39a3dcc",
+    "url": "/kittyfriends/static/js/runtime~main.ecbdd3d7.js"
+  },
+  {
+    "revision": "2ae5e01725850d009ae7",
+    "url": "/kittyfriends/static/js/main.2ae5e017.chunk.js"
+  },
+  {
+    "revision": "fdc33cf32f57a45f0b54",
+    "url": "/kittyfriends/static/js/1.fdc33cf3.chunk.js"
+  },
+  {
+    "revision": "2ae5e01725850d009ae7",
+    "url": "/kittyfriends/static/css/main.5bb87357.chunk.css"
+  },
+  {
+    "revision": "fdc33cf32f57a45f0b54",
+    "url": "/kittyfriends/static/css/1.9652a668.chunk.css"
+  },
+  {
+    "revision": "f1b085313b25b0f51cffa1af5c5b9b48",
+    "url": "/kittyfriends/index.html"
+  }
+];
