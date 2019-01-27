9dfa63616ae3ea879cc434a38c75bbc5040b8264
evgner <evgner@gmail.com>
2019-01-27 18:59:59 +0600

    Add redux-thunk for async actions

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,7 +7,7 @@ import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-import { setSearchField } from "../actions";
+import { setSearchField, requestKitties } from "../actions";
 
 /*import mapStateToProps
 	from "react-redux/es/connect/mapStateToProps";
@@ -16,18 +16,22 @@ import mapDispatchToProps
 
 const mapStateToProps = state => {
 	return {
-		searchField: state.searchField
+		searchField: state.searchKitties.searchField,
+		kitties: state.requestKitties.kitties,
+		isPending: state.requestKitties.isPending,
+		error: state.requestKitties.error
 	}
 };
 
 const mapDispatchToProps = ( dispatch ) => {
 	return {
-		onSearchChange: ( event ) => dispatch( setSearchField( event.target.value ) )
+		onSearchChange: ( event ) => dispatch( setSearchField( event.target.value ) ),
+		onRequestKitties: () => dispatch( requestKitties() ) // requestKitties( dispatch )
 	}
 };
 
 class App extends Component {
-
+/*
 	constructor() {
 		super();
 		this.state = {
@@ -35,14 +39,18 @@ class App extends Component {
 			// searchfield: ''
 		}
 	}
+*/
 
 	componentDidMount() {
 		// console.log( this.props.store.getState() );
 		// this.setState( { kitties: kitties } );
 		// console.log( this.state );
-		fetch( 'https://jsonplaceholder.typicode.com/users' )
-			.then( response => response.json() )
-			.then( users => this.setState( { kitties: users } ) )
+
+		// fetch( 'https://jsonplaceholder.typicode.com/users' )
+		// 	.then( response => response.json() )
+		// 	.then( users => this.setState( { kitties: users } ) )
+
+		this.props.onRequestKitties();
 	}
 
 /*
@@ -52,12 +60,12 @@ class App extends Component {
 */
 
 	render() {
-		const { kitties } = this.state;
-		const { searchField, onSearchChange } = this.props;
+		// const { kitties } = this.state;
+		const { searchField, onSearchChange, kitties, isPending } = this.props;
 		const filteredKitties = kitties.filter( kitty => {
 			return kitty.name.toLowerCase().includes( searchField.toLowerCase() );
 		});
-		return ! kitties.length ?
+		return isPending ?
 			<h1 className={'tc'}>Loading...</h1> :
 		(
 			<div className={'tc'}>
